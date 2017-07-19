27671567849592350a55c1ab0bca5ef9e42a303c
manit <ermanit007@gmail.com>
2017-07-19 13:03:41 +0530

    Renamed variable name for better readability click on listener on textview to change cardview elevation textview margin set

--- app/src/main/java/com/grademojo/cardview/MainActivity.java
+++ app/src/main/java/com/grademojo/cardview/MainActivity.java
@@ -2,30 +2,58 @@ package com.grademojo.cardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView eleVator10 ,eleVator20 ,eleVator30 ,eleVator0;
+    private TextView textEleVator10, textEleVator20, textElevator30, textEleVator0;
+    private CardView mainCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        eleVator10 = (TextView) findViewById(R.id.first);
-        eleVator20 = (TextView) findViewById(R.id.second);
-        eleVator30 = (TextView) findViewById(R.id.third);
-        eleVator0 = (TextView) findViewById(R.id.fourth);
+        textEleVator10 = (TextView) findViewById(R.id.first);
+        textEleVator20 = (TextView) findViewById(R.id.second);
+        textElevator30 = (TextView) findViewById(R.id.third);
+        textEleVator0 = (TextView) findViewById(R.id.fourth);
+
+        mainCardView = (CardView) findViewById(R.id.card);
 
 
 
 
-        eleVator10.setOnClickListener(new View.OnClickListener() {
+        textEleVator10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                     mainCardView.setCardElevation(10);
+            }
+        });
 
+
+        textEleVator20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainCardView.setCardElevation(20);
+            }
+        });
+
+
+        textElevator30.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainCardView.setCardElevation(30);
+            }
+        });
+
+
+        textEleVator0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainCardView.setCardElevation(0);
             }
         });
     }
